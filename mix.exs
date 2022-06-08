e6f0613b597940d4821a7147b5383e4959459f06
Alisson Morais <alissonfpmorais@gmail.com>
2022-06-08 20:48:18 -0300

    feat: add publishing artifacts

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule CreditCard.MixProject do
   def project do
     [
       app: :credit_card,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "This project aims to help with credit card validation and utilities",
+      source_url: "https://github.com/alissonfpmorais/credit_card",
+      package: package()
     ]
   end
 
@@ -22,7 +25,16 @@ defmodule CreditCard.MixProject do
   defp deps do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:luhn, "~> 0.3.0"}
+    ]
+  end
+
+  defp package do
+    [
+      name: "credit_card_utils",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/alissonfpmorais/credit_card"}
     ]
   end
 end
