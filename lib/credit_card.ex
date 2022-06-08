e3c0e40f8f1d515e61a2f86194d78c9705d489d9
Alisson Morais <alissonfpmorais@gmail.com>
2022-06-08 16:05:40 -0300

    feat: initial commit

---
+++ lib/credit_card.ex
@@ -0,0 +1,18 @@
+defmodule CreditCard do
+  @moduledoc """
+  Documentation for `CreditCard`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CreditCard.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/credit_card_test.exs
@@ -0,0 +1,8 @@
+defmodule CreditCardTest do
+  use ExUnit.Case
+  doctest CreditCard
+
+  test "greets the world" do
+    assert CreditCard.hello() == :world
+  end
+end
